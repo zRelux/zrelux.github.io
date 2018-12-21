dbd646394d8e9652728e950fcc8baf5c359f7ae8
zRelux <leonard.drici@gmail.com>
2018-12-21 17:47:12 +0000

    Updates

---
+++ precache-manifest.fe323fb7ab7b94251aeab23da7348ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7078add747e3e7a9e208",
+    "url": "/static/js/main.7078add7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7078add747e3e7a9e208",
+    "url": "/static/css/main.3270ed76.chunk.css"
+  },
+  {
+    "revision": "2df6006ac427c94de6327520675424a0",
+    "url": "/index.html"
+  }
+];
